17f0fc268bee11a9d1c88b7ecbabe64a7955378c
Preeti Rawat <preetirawat711@gmail.com>
2021-07-26 14:58:55 +0530

    Day3: Intro to Conditional Statements

---
+++ src/academy/learnprogramming/Day3.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if(N%2==0){
+            if(N>=2&&N<=5){
+                System.out.println("Not Weird");
+            }else if(N>=6&&N<=20){
+                System.out.println("Weird");
+            }else if(N>20){
+                System.out.println("Not Weird");
+            }
+        }else{
+            System.out.println("Weird");
+        }
+        bufferedReader.close();
+    }
+
+}
