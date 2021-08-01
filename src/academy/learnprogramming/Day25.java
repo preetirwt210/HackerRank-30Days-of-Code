9a91f9e63b2e865b674a7f7b34cae72ff38d6b29
Preeti Rawat <preetirawat711@gmail.com>
2021-08-01 16:05:40 +0530

    Day25: Running Time and Complexity

---
+++ src/academy/learnprogramming/Day25.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Day25 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT.
+        Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            if (isPrime(sc.nextInt()))
+                System.out.println("Prime");
+            else
+                System.out.println("Not prime");
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num == 1) return false;
+        for (int i = 2; i < Math.sqrt(num); i++)
+            if (num % i == 0) return false;
+        return true;
+    }
+
+}
+
